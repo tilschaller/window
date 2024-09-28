139d23ef31623d5ccb44e9176849c3345d47b4b6
Til Schaller <schaller.til@gmail.com>
2024-09-28 21:02:12 +0200

    fixed core dump

--- main.cpp
+++ main.cpp
@@ -22,7 +22,8 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 // camera - give pretty starting point
-Camera camera(glm::vec3(67.0f, 627.5f, 169.9f), glm::vec3(0.0f, 1.0f, 0.0f), -128.1f, -42.4f);
+Camera camera(glm::vec3(67.0f, 627.5f, 169.9f), glm::vec3(0.0f, 1.0f, 0.0f),
+              -128.1f, -42.4f);
 
 float lastX;
 float lastY;
@@ -34,121 +35,120 @@ void processInput(GLFWwindow *window);
 void mouse_callback(GLFWwindow *window, double xposIn, double yposIn);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
-int main(void)
-{
-    stbi_set_flip_vertically_on_load(true);
+int main(void) {
+  stbi_set_flip_vertically_on_load(true);
 
-    if (!glfwInit())
-    {
-        std::runtime_error("could not initialize glfw");
-    }
+  if (!glfwInit()) {
+    std::runtime_error("could not initialize glfw");
+  }
 
-    glfwSetErrorCallback(error_callback);
+  glfwSetErrorCallback(error_callback);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
-    if (!window)
-    {
-        std::runtime_error("could not create glfw window");
-    }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  GLFWwindow *window =
+      glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+  if (!window) {
+    std::runtime_error("could not create glfw window");
+  }
 
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
 
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    GLint maxTessLevel;
-    glGetIntegerv(GL_MAX_TESS_GEN_LEVEL, &maxTessLevel);
-    std::cout << "Max available tess level: " << maxTessLevel << std::endl;
+  GLint maxTessLevel;
+  glGetIntegerv(GL_MAX_TESS_GEN_LEVEL, &maxTessLevel);
+  std::cout << "Max available tess level: " << maxTessLevel << std::endl;
 
-    glEnable(GL_DEPTH_TEST);
+  glEnable(GL_DEPTH_TEST);
 
-    Shader shader("shader/vertex.vs", "shader/fragment.fs", nullptr, "shader/tessControl.tcs", "shader/tessEval.tes");
+  Shader shader("shader/vertex.vs", "shader/fragment.fs", nullptr,
+                "shader/tessControl.tcs", "shader/tessEval.tes");
 
-    Terrain terrain("assets/iceland_heightmap.png", &shader);
+  // it needs to be allocated dynamically in order to be destroy before
+  // glfwTerminate
+  Terrain *terrain = new Terrain("assets/iceland_heightmap.png", &shader);
 
-    while (!glfwWindowShouldClose(window))
-    {
-        float currentFrame = static_cast<float>(glfwGetTime());
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
+  while (!glfwWindowShouldClose(window)) {
+    float currentFrame = static_cast<float>(glfwGetTime());
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
 
-        processInput(window);
+    processInput(window);
 
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        shader.use();
+    shader.use();
 
-        glm::mat4 projection =
-            glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100000.0f);
-        glm::mat4 view = camera.GetViewMatrix();
-        shader.setMat4("projection", projection);
-        shader.setMat4("view", view);
+    glm::mat4 projection =
+        glm::perspective(glm::radians(camera.Zoom),
+                         (float)WIDTH / (float)HEIGHT, 0.1f, 100000.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    shader.setMat4("projection", projection);
+    shader.setMat4("view", view);
 
-        // world transformation
-        glm::mat4 model = glm::mat4(1.0f);
-        shader.setMat4("model", model);
+    // world transformation
+    glm::mat4 model = glm::mat4(1.0f);
+    shader.setMat4("model", model);
 
-        // render the cube
-        glBindVertexArray(terrain.terrainVAO);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glDrawArrays(GL_PATCHES, 0, terrain.NUM_PATCH_PTS * terrain.rez * terrain.rez);
+    // render the cube
+    glBindVertexArray(terrain->terrainVAO);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glDrawArrays(GL_PATCHES, 0,
+                 terrain->NUM_PATCH_PTS * terrain->rez * terrain->rez);
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
 
-    glfwDestroyWindow(window);
-    // this throws a segfault under Wayland for some reason
-    glfwTerminate();
+  delete terrain;
 
-    return 0;
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
 }
 
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime);
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    camera.ProcessKeyboard(FORWARD, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    camera.ProcessKeyboard(BACKWARD, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    camera.ProcessKeyboard(LEFT, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
-void mouse_callback(GLFWwindow *window, double xposIn, double yposIn)
-{
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
+void mouse_callback(GLFWwindow *window, double xposIn, double yposIn) {
+  float xpos = static_cast<float>(xposIn);
+  float ypos = static_cast<float>(yposIn);
 
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
+  if (firstMouse) {
     lastX = xpos;
     lastY = ypos;
+    firstMouse = false;
+  }
 
-    camera.ProcessMouseMovement(xoffset, yoffset);
+  float xoffset = xpos - lastX;
+  float yoffset =
+      lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+  lastX = xpos;
+  lastY = ypos;
+
+  camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-    glViewport(0, 0, width, height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
 
-void error_callback(int error, const char *description)
-{
-    std::cout << "Error: " + std::to_string(*description) << std::endl;
+void error_callback(int error, const char *description) {
+  std::cout << "Error: " + std::to_string(*description) << std::endl;
 }
